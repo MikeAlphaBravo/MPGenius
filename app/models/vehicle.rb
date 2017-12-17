cdf59a4fe9541bf04b977eeaae75919cafd243af
Michael Brooks <mikeyabrooks@gmail.com>
2017-12-17 13:32:35 -0800

    initiate all seeds in file confirmed working

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,8 @@
 class Vehicle < ApplicationRecord
   has_many :accounts
   has_many :users, through: :accounts
+  has_many :fill_ups
+  has_many :services
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ class Seed
     seed = Seed.new
     seed.generate_users
     seed.generate_vehicles
-    # seed.generate_accounts
+    seed.generate_accounts
     seed.generate_fill_ups
     seed.generate_services
   end
@@ -37,24 +37,25 @@ class Seed
     end
   end
 
-  # def generate_accounts
-  #   Accounts.destroy_all
-  #   Vehicle.all.each do |i|
-  #     user = User.find(rand(User.first.id..User.last.id))
-  #     accounts = Accounts.create!(
-  #       user_id: user.id,
-  #       vehicle_id: i.id
-  #     )
-  #     puts "Account Generated #{accounts.id}"
-  #   end
-  # end
+  def generate_accounts
+    Account.destroy_all
+    Vehicle.all.each do |i|
+      user = User.find(rand(User.first.id..User.last.id))
+      accounts = Account.create!(
+        user_id: user.id,
+        vehicle_id: i.id
+      )
+      puts "Account Generated #{accounts.id}"
+    end
+  end
 
   def generate_fill_ups
+    FillUp.destroy_all
     100.times do |i|
       vehicle = Vehicle.find(rand(Vehicle.first.id..Vehicle.last.id))
       user = vehicle.users.first
       # vehicle.fill_ups.create!(
-      fill_up = FillUp.create!(
+      fill_ups = FillUp.create!(
         odometer_reading: Faker::Number.between(0, 250000),
         price_per_gallon: Faker::Number.decimal(2),
         gallons_in_fill: Faker::Number.decimal(2),
@@ -64,23 +65,24 @@ class Seed
         notes: Faker::Hipster.sentence,
         vehicle_id: vehicle.id
       )
-      puts "Fill Ups Generated #{vehicle.fill_ups.last.nickname}"
+      puts "Fill Ups Generated #{vehicle.fill_ups.last.created_at}"
     end
   end
 
   def generate_services
+    Service.destroy_all
     20.times do |i|
       vehicle = Vehicle.find(rand(Vehicle.first.id..Vehicle.last.id))
       user = vehicle.users.first
       # vehicle.services.create!(
-      service = Service.create!(
+      services = Service.create!(
         odometer_reading: Faker::Number.between(0, 250000),
-        total_cost: Faker::Number..decimal(2),
+        total_cost: Faker::Number.decimal(2),
         # .between(0, 100),
         notes: Faker::Hipster.sentence,
-        # vehicle_id: vehicle.id
+        vehicle_id: vehicle.id
       )
-      puts "Services Generated #{vehicle.services.last.nickname}"
+      puts "Services Generated #{vehicle.services.last.created_at}"
     end
   end
 end
